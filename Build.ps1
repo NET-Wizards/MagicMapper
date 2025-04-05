cb905e9748caacc9bbdddc5846a364fd4a57f257
Andrii Kurdiumov <kant2002@gmail.com>
2025-04-05 19:58:03 +0500

    Move to FastMapper package name Change README to include new name, point to relationship with AutoMapper. Configure Myget feed

--- Build.ps1
+++ Build.ps1
@@ -26,6 +26,6 @@ $artifacts = ".\artifacts"
 
 if(Test-Path $artifacts) { Remove-Item $artifacts -Force -Recurse }
 
-exec { & dotnet test -c Release --results-directory $artifacts -l trx }
+exec { & dotnet test -c Release --results-directory $artifacts -l trx /bl:$artifacts\FastMapper.binlog }
 
 exec { & dotnet pack .\src\AutoMapper\AutoMapper.csproj -c Release -o $artifacts --no-build }
